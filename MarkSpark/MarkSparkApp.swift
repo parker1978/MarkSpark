765e2c860495e94c691861b37f1a2a64e8eaad0f
Stephen Parker <parker1978@me.com>
2025-08-09 08:42:16 -0500

    Initial Commit

---
+++ MarkSpark/MarkSparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkSparkApp.swift
+//  MarkSpark
+//
+//  Created by Stephen Parker on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct MarkSparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
